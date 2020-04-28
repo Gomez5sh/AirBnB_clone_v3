72a5ff7f74e98026996b30ac527d7013a7a1f7a4
sebastianchc <1200@holbertonschool.com>
2020-04-28 15:46:41 -0500

    Staring with API

---
+++ api/v1/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+It’s time to start your API!
+"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+if __name__ == "__main__":
+    HBNB_API_HOST = getenv("HBNB_API_HOST")
+    HBNB_API_PORT = getenv("HBNB_API_PORT")
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
