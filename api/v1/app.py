a3252cdcf9a4f15e67c3f88ab749e5a45c474c5a
g5sh <eagomez2387@gmail.com>
2020-04-28 16:56:50 -0500

    [ADD]DIRECTORYS: Add directorys and init files

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 It’s time to start your API!
 """
-from flask import Flask
+from flask import Flask, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -16,6 +16,6 @@ def teardown_db(exception):
     storage.close()
 
 if __name__ == "__main__":
-    HBNB_API_HOST = getenv("HBNB_API_HOST")
-    HBNB_API_PORT = getenv("HBNB_API_PORT")
+    HBNB_API_HOST = getenv("HBNB_API_HOST", default="0.0.0.0")
+    HBNB_API_PORT = getenv("HBNB_API_PORT", default=5000)
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
-    return {"status": "OK"}
+    return jsonify({"status": "OK"})
