9d44b5185dd6a1c99987daac609b89bb169600c4
g5sh <eagomez2387@gmail.com>
2020-04-29 15:39:20 -0500

    [ADD]CITY: Create citys

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,36 +9,36 @@ from models.state import City
 
 @app_views.route("/states/<state_id>/cities", methods=["GET"], strict_slashes=False)
 @app_views.route("/cities", methods=["GET"], strict_slashes=False)
-def get_states(city_id=None):
+def get_cities(city_id=None):
     """GET request"""
-    states_list = []
+    City_list = []
     if not state_id:
         states = storage.all(State).values()
-        for state in states:
-            states_list.append(state.to_dict())
+        for city in states.city:
+            City_list.append(city.to_dict())
         return jsonify(states_list)
     else:
-        state = storage.get(State, state_id)
+        city = storage.get(City, city_id)
         if not state:
             abort(404)
-        return jsonify(state.to_dict())
+        return jsonify(city.to_dict())
 
 
 @app_views.route("/cities/<city_id>/",
                  methods=["DELETE"],
                  strict_slashes=False)
-def delete_states(city_id):
+def delete_cities(city_id):
     """DELETE request"""
-    state = storage.get(State, state_id)
-    if not state:
+    city = storage.get(City, city_id)
+    if not city:
         abort(404)
-    state.delete()
+    city.delete()
     storage.save()
     return jsonify({}), 200
 
 
 @app_views.route("/states/<state_id>/cities", methods=["POST"], strict_slashes=False)
-def post_states():
+def post_city(state_id):
     """POST request"""
     info = request.get_json()
     if not info:
@@ -51,16 +51,16 @@ def post_states():
 
 
 @app_views.route("/cities/<city_id>", methods=["PUT"], strict_slashes=False)
-def put_states(city_id):
+def put_city(city_id):
     """PUT request"""
-    state = storage.get(State, state_id)
-    if not state:
+    state = storage.get(City, city_id)
+    if not city:
         abort(404)
     info = request.get_json()
     if not info:
         abort(400, "Not a JSON")
     for key, value in info.items():
         if key not in ["id", "created_at", "updated_at"]:
-            setattr(state, key, value)
+            setattr(city, key, value)
     storage.save()
-    return jsonify(state.to_dict()), 200
+    return jsonify(city.to_dict()), 200
