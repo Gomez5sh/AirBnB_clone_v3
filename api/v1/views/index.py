75a4fefa2a51dea731703228ab7bb196ceec958b
sebastianchc <1200@holbertonschool.com>
2020-04-28 19:40:36 -0500

    work...pls

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status", strict_slashes=False)
@@ -11,12 +17,12 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/api/v1/stats", strict_slashes=False)
+@app_views.route("/stats", strict_slashes=False)
 def stats():
     """Stats"""
     classes = [Amenity, City, Place, Review, State, User]
     keys = ["amenities", "cities", "places", "reviews", "states", "users"]
     stats_count = {}
-    for idx in range(len(clases)):
+    for idx in range(len(classes)):
         stats_count[keys[idx]] = storage.count(classes[idx])
     return jsonify(stats_count)
